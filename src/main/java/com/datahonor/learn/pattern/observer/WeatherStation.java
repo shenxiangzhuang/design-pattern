0cd4537fb494af5dc7e10463fe4cdf31706d8f51
Xiangzhuang Shen <datahonor@gmail.com>
2023-04-17 23:48:50 +0800

    Added:observer pattern

---
+++ src/main/java/com/datahonor/learn/pattern/observer/WeatherStation.java
@@ -0,0 +1,13 @@
+package com.datahonor.learn.pattern.observer;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(79, 90, 29.2f);
+    }
+}
